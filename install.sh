a348338fb17fe61b36c6026baf337312b0680eda
antonio <tonyldo@gmail.com>
2017-09-25 20:40:50 -0300

    readme

--- install.sh
+++ install.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-rm $HOME/.mosquitto/data/ $HOME/.mosquitto/config $HOME/.mosquitto/log $HOME/.homeassistant/config
+echo "Create directories..."
+rm $HOME/.mosquitto/data/ $HOME/.mosquitto/config $HOME/.mosquitto/log $HOME/.homeassistant/config -r
 mkdir $HOME/.mosquitto/data/ $HOME/.mosquitto/config $HOME/.mosquitto/log $HOME/.homeassistant/config
 
+echo "Copy config file for mosquitto..."
 cp ./config/mosquitto.conf $HOME/.mosquitto/config
 
+echo "Run Containers..."
 docker stop home-assistant mosquitto
 
-docker system prune home-assistant mosquitto
+docker system prune -a
 
 docker run -d --user $UID:$GROUPS --name="mosquitto" -p 1883:1883 -p 9001:9001 -v $HOME/.mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v $HOME/.mosquitto/data/:/mosquitto/data/ -v $HOME/.mosquitto/log/:/mosquitto/log/ eclipse-mosquitto
 
 docker run -d --user $UID:$GROUPS --name="home-assistant" -p 8123:8123 -v $HOME/.homeassistant/config/:/config -v /etc/localtime/:/etc/localtime:ro homeassistant/home-assistant
+
+echo "Integrating HASS and Mosquitto..."
+
+while [ ! -f $HOME/.homeassistant/config/configuration.yaml ]
+do
+  sleep 2
+done
+
+ls -l $HOME/.homeassistant/config/configuration.yaml
 
 echo "mqtt:" >> $HOME/.homeassistant/config/configuration.yaml
 
 echo " broker: 172.17.0.1" >> $HOME/.homeassistant/config/configuration.yaml
 
 docker restart home-assistant
+
+echo "Finish."
+

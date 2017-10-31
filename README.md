fdb97037f67d99e974fe44535afeeaaa66d18434
antonio <tonyldo@gmail.com>
2017-10-30 23:09:32 -0300

    Mosquitto security

--- README.md
+++ README.md
@@ -17,12 +17,12 @@ $ cd /path/to/dir/home-automation-mqtt-hass
 
 $ chmod +x install.sh
 
-$ ./install.sh
+$ ./install.sh $MOSQUITTO_USER $MOSQUITTO_USER_PASS
 
 # Testing
 
 $ curl -X POST -H "Content-Type: application/json" -d '{"payload": "Teste", "topic": "hello/world", "retain": "True"}' http://localhost:8123/api/services/mqtt/publish
 
-$ mosquitto_sub -t hello/world -q 2
+$ mosquitto_sub -u $MOSQUITTO_USER -P $MOSQUITTO_USER_PASS -t hello/world -q 2
 
 Go to https://github.com/tonyldo/home-assistant-device-simulator for futher information how to test other Home Assistant features.
--- install.sh
+++ install.sh
@@ -1,37 +1,55 @@
 #!/bin/bash
 
+echo "Stop Containers..."
+docker stop home-assistant mosquitto
+
 echo "Create directories..."
-rm $HOME/.mosquitto/data/ $HOME/.mosquitto/config $HOME/.mosquitto/log $HOME/.homeassistant/config -r
-mkdir $HOME/.mosquitto/data/ $HOME/.mosquitto/config $HOME/.mosquitto/log $HOME/.homeassistant/config
+rm $HOME/.mosquitto/data/ $HOME/.mosquitto/config $HOME/.mosquitto/log $HOME/.homeassistant/config $HOME/.mosquitto/pwd -r
+mkdir $HOME/.mosquitto/data/ $HOME/.mosquitto/config $HOME/.mosquitto/log $HOME/.homeassistant/config $HOME/.mosquitto/pwd
+
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
 
 echo "Copy config file for mosquitto..."
-cp ./config/mosquitto.conf $HOME/.mosquitto/config
-
-echo "Run Containers..."
-docker stop home-assistant mosquitto
+cp $SCRIPTPATH/config/mosquitto.conf $HOME/.mosquitto/config
 
 docker system prune -a
 
-docker run -d --user $UID:$GROUPS --name="mosquitto" -p 1883:1883 -p 9001:9001 -v $HOME/.mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v $HOME/.mosquitto/data/:/mosquitto/data/ -v $HOME/.mosquitto/log/:/mosquitto/log/ eclipse-mosquitto
+docker run -d --user $UID:$GROUPS --name="mosquitto" -p 1883:1883 -p 9001:9001 -v $HOME/.mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v $HOME/.mosquitto/data/:/mosquitto/data/ -v $HOME/.mosquitto/log/:/mosquitto/log/ -v $HOME/.mosquitto/pwd/:/mosquitto/pwd/ eclipse-mosquitto
 
 docker run -d --user $UID:$GROUPS --name="home-assistant" -p 8123:8123 -v $HOME/.homeassistant/config/:/config -v /etc/localtime/:/etc/localtime:ro homeassistant/home-assistant
 
-echo "Integrating HASS and Mosquitto..."
+echo "Create HASS config..."
 
 while [ ! -f $HOME/.homeassistant/config/configuration.yaml ]
 do
   sleep 5
 done
 
+echo "HASS configuration file created..."
 ls -l $HOME/.homeassistant/config/configuration.yaml
 
+echo "Create mosquitto security file..."
+docker exec mosquitto touch "/mosquitto/pwd/pass.file"
+docker exec mosquitto mosquitto_passwd -b "/mosquitto/pwd/pass.file" "$1" "$2"
+echo "allow_anonymous false" >> $HOME/.mosquitto/config/mosquitto.conf
+echo "password_file /mosquitto/pwd/pass.file" >> $HOME/.mosquitto/config/mosquitto.conf
+
+echo "Integrating HASS and Mosquitto..."
 echo " " >> $HOME/.homeassistant/config/configuration.yaml
 
 echo "mqtt:" >> $HOME/.homeassistant/config/configuration.yaml
 
 echo " broker: 172.17.0.1" >> $HOME/.homeassistant/config/configuration.yaml
+echo " username: $1" >> $HOME/.homeassistant/config/configuration.yaml
+echo " password: $2" >> $HOME/.homeassistant/config/configuration.yaml
 
-docker restart home-assistant
+echo " discovery: true" >> $HOME/.homeassistant/config/configuration.yaml
+echo " discovery_prefix: homeassistant" >> $HOME/.homeassistant/config/configuration.yaml
+
+docker restart home-assistant mosquitto
 
 echo "Finish."
 

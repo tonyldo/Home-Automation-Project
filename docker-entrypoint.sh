0113265f26b3a5571f77954ef67c7f310d09767c
ANTONIO CAMPOS <tonyldo@gmail.com>
2018-12-03 13:14:15 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-if ( [ -z "${MOSQUITTO_USERNAME}" ] || [ -z "${MOSQUITTO_PASSWORD}" ] ); then
-  echo "MOSQUITTO_USERNAME or MOSQUITTO_PASSWORD not defined"
-  exit 1
-fi
-
 echo "persistence true"  >> /mosquitto/config/mosquitto.conf
 echo "persistence_location /mosquitto/data/" >> /mosquitto/config/mosquitto.conf
 echo "log_dest file /mosquitto/log/mosquitto.log" >> /mosquitto/config/mosquitto.conf
 
-echo "allow_anonymous false" >> /mosquitto/config/mosquitto.conf
-echo "password_file /mosquitto/pwd/passwordfile" >> /mosquitto/config/mosquitto.conf
+if ( [ -z "${MOSQUITTO_USERNAME}" ] || [ -z "${MOSQUITTO_PASSWORD}" ] ); 
+  then
+    echo "MOSQUITTO_USERNAME or MOSQUITTO_PASSWORD not defined"
+  else
+    echo "allow_anonymous false" >> /mosquitto/config/mosquitto.conf
+    echo "password_file /mosquitto/pwd/passwordfile" >> /mosquitto/config/mosquitto.conf    
+fi
 
 # create mosquitto passwordfile
 touch passwordfile

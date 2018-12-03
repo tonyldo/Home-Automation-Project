0ddfc5f52ac94f980f717a1430742f96feaf1671
ANTONIO CAMPOS <tonyldo@gmail.com>
2018-12-03 14:46:56 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,20 @@
 #!/bin/ash
 
 set -e
+conf_file=/mosquitto/config/mosquitto.conf
 
-echo "persistence true"  >> /mosquitto/config/mosquitto.conf
-echo "persistence_location /mosquitto/data/" >> /mosquitto/config/mosquitto.conf
-echo "log_dest file /mosquitto/log/mosquitto.log" >> /mosquitto/config/mosquitto.conf
+echo "persistence true"  >> conf_file
+echo "persistence_location /mosquitto/data/" >> conf_file
+echo "log_dest file /mosquitto/log/mosquitto.log" >> conf_file
 
 if ( [ -z "${MOSQUITTO_USERNAME}" ] || [ -z "${MOSQUITTO_PASSWORD}" ] ); 
   then
     echo "MOSQUITTO_USERNAME or MOSQUITTO_PASSWORD not defined"
   else
-    echo "allow_anonymous false" >> /mosquitto/config/mosquitto.conf
-    echo "password_file /mosquitto/pwd/passwordfile" >> /mosquitto/config/mosquitto.conf    
+    # create mosquitto passwordfile
+    touch passwordfile
+    mosquitto_passwd -b /mosquitto/pwd/passwordfile $MOSQUITTO_USERNAME $MOSQUITTO_PASSWORD
+    sed -i 's/\#allow_anonymous\ true/allow_anonymous\ false/' $conf_file    
 fi
-
-# create mosquitto passwordfile
-touch passwordfile
-mosquitto_passwd -b /mosquitto/pwd/passwordfile $MOSQUITTO_USERNAME $MOSQUITTO_PASSWORD
 
 exec "$@"
